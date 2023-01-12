273fcd61cf161776623981790a61f33a81cae206
Kenneth Rey Tamis <teknolikha@gmail.com>
2023-01-12 13:18:37 +0800

    initial commit

--- font/font.js
+++
@@ -1,7 +0,0 @@
-const getValueInput = () => {
-  let inputValue = document.getElementById('textSample').value;
-  let selectedValue = document.getElementById('font').value;
-  document.getElementById('valueInput').innerHTML = inputValue;
-  document.getElementById('valueInput').className =
-    selectedValue + ' display-1';
-};
